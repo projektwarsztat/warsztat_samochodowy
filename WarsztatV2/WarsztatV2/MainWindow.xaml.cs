32836ba2b0d7295b8dfcb14c5ca8952b29c250ce
acieszynski <adam.cieszynski86@gmail.com>
2022-03-23 20:43:51 +0100

    Stworzenie szkieletu aplikacji
    1. Menu i możliwość przełączania pomiędzy zakładkami klienci i o firmie.
    2. Stworzenie Formularzy (tylko xaml) do dodawania klientów i informacji o firmie.

---
+++ WarsztatV2/WarsztatV2/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WarsztatV2.Menu;
+
+namespace WarsztatV2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+       
+
+        //private void Akk(object sender, RoutedEventArgs e, ContentControl contentControl) => contentControl.Content = new page2();
+
+     
+
+
+        private void OFirmieClick(object sender, RoutedEventArgs e)
+        {
+            
+            RightContent.Content = new OFirmie();
+        }
+
+        private void KlienciClick(object sender, RoutedEventArgs e)
+        {
+
+            RightContent.Content = new Klienci();
+        }
+
+
+    }
+}

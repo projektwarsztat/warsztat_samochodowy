8509d3ec52a79cf93a99d6f0f3271ef243789618
acieszynski <adam.cieszynski86@gmail.com>
2022-03-30 16:53:29 +0200

    Nowe Menu
    Dodanie menu rozwijanego i panelu powitalnego użytkownika

--- WarsztatV2/WarsztatV2/MainWindow.xaml.cs
+++ WarsztatV2/WarsztatV2/MainWindow.xaml.cs
@@ -11,14 +11,37 @@ namespace WarsztatV2
         public MainWindow()
         {
             InitializeComponent();
+            HideSubmenu();
+            guzik.Text = "Witaj ! \nJan kowalski";
         }
 
+        private void ShowSubmenu()
+        {       
+          if(NoweZlecenie.IsVisible==true)
+            HideSubmenu();
 
+          else
+            {
+                NoweZlecenie.Visibility = Visibility.Visible;
+                DoNaprawy.Visibility = Visibility.Visible;
+                DoOdbioru.Visibility = Visibility.Visible;
+                HistoriaZlecen.Visibility = Visibility.Visible;
+            }
+        }
 
-        //private void Akk(object sender, RoutedEventArgs e, ContentControl contentControl) => contentControl.Content = new page2();
+        private void HideSubmenu()
+        {
+            NoweZlecenie.Visibility = Visibility.Collapsed;
+            DoNaprawy.Visibility = Visibility.Collapsed;
+            DoOdbioru.Visibility = Visibility.Collapsed;
+            HistoriaZlecen.Visibility = Visibility.Collapsed;
 
+        }
 
-
+        private void ZleceniaClick(object sender, RoutedEventArgs e)
+        {
+           ShowSubmenu();
+        }
 
         private void OFirmieClick(object sender, RoutedEventArgs e)
         {
